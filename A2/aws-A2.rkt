c546c99d5a9313f5d5cb79371eabec4eb82c0e22
awsalmah <awsalmahasneh@gmail.com>
2026-01-25 16:17:13 -0500

    Added cond

---
+++ A2/aws-A2.rkt
@@ -0,0 +1,155 @@
+#lang racket
+
+;; An Expression is one of:
+;; - (if Expression Expression Expression)
+;; - (lambda (Var) Expression)
+;; - Variable -- just symbols
+;; - Number
+;; - (Expression Expression ...)
+;; - (MACRO S-Expression ...)
+
+;; - An S-Expression is one of:
+;; - Number
+;; - Symbol
+;; - (S-Expression ...)
+
+(define the-macros (make-hash))
+(define (macro? m) (hash-ref the-macros m #f))
+(define (get-macro m) (hash-ref the-macros m))
+
+(define (extend-syntax! m proc)
+  (hash-set! the-macros m proc))
+
+
+;; expand : S-Expression -> Expression
+;; expand all the macros
+(define (expand s)
+  (match s
+    [(? number?) s]
+    [(? symbol?) s]
+    [`(lambda (,v) ,body)
+     `(lambda (,v) ,(expand body))]
+    [`(if ,tst ,thn ,els)
+     `(if ,(expand tst) ,(expand thn) ,(expand els))]
+    [`(,(? macro? m) ,operand ...)
+     (expand ((get-macro m) s))]
+    [`(,op ,operand ...)
+     `(,(expand op) ,@(map expand operand))]
+    [(? void?) s])) ;;;;;;; added ;;;;;;;
+
+(extend-syntax! 'or
+                (lambda (s)
+                  (match s
+                    [`(or ,a ,b)
+                     `(let ([x ,a])
+                        (if x
+                            x
+                            ,b))]
+                    [`(or ,a ,b ,c)
+                     `(or ,a (or ,b ,c))])))
+
+
+
+(extend-syntax! 'let
+                (λ (s)
+                  (match s
+                    [`(let ((,x ,v)) ,b)
+                     `((lambda (,x) ,b) ,v)])))
+
+
+
+(define (interp s)
+  (let loop ([s (expand s)])
+    (match s
+      [`(if ,(app loop 0) ,e1 ,e2) (loop e2)]
+      [`(if ,(app loop x) ,e1 ,e2) (loop e1)]
+      [`(lambda (,x) ,body)
+       (lambda (v) (loop (subst x v body)))]
+      #;[`(,(? macro? m) ,operand ...)
+         (interp ((get-macro m) s))]
+      [`(,f ,e ...) (apply (loop f) (map loop e))]
+      [(? number?) s]
+      [(? void?) s] ;;;;;;; added ;;;;;;;
+      )))
+
+(define (subst x v s)
+  (match s
+    [(== x) v]
+    [`(if ,e1 ,e2 ,e3) `(if ,(subst x v e1)
+                            ,(subst x v e2)
+                            ,(subst x v e3))]
+    [`(lambda (,x0) ,b)
+     (if (eq? x0 x)
+         s
+         `(lambda (,x0)
+            ,(subst x v b)))]
+    [`+ +]
+    [(list e ...) (map (lambda (e) (subst x v e)) e)]
+    [_ s]))
+
+
+(interp '(let ([x 1]) (or x (+ x 2))))
+
+(interp '((lambda (x) (or x 2 y)) 0))
+
+(interp `(let ([x 10]) (or 0 x)))
+
+(extend-syntax! 'cond
+                (λ (s)
+                  (match s
+                    [`(cond)
+                     `,(void)]
+                    [`(cond
+                        [else ,S-expression1-2])
+                     `,S-expression1-2]
+                    
+                    [`(cond
+                        [,S-expression1-1 ,S-expression1-2])
+                     `(if ,S-expression1-1
+                          ,S-expression1-2
+                          ,(void))]
+                    
+                    [`(cond
+                        [,S-expression1-1 ,S-expression1-2]
+                        ,exprs ...
+                        )
+                     `(if ,S-expression1-1
+                          ,S-expression1-2
+                          (cond ,@exprs))]
+                    )))
+
+(require rackunit)
+
+(check-equal? (interp `(cond)) (void))
+(check-equal? (interp `(cond
+                         [else 0])) 0)
+(check-equal? (interp `(cond
+                         [0 1])) (void))
+(check-equal? (interp `(cond
+                         [1 1])) 1)
+(check-equal? (interp `(cond
+                         [0 1]
+                         [0 2])) (void))
+(check-equal? (interp `(cond
+                         [0 1]
+                         [1 2])) 2)
+(check-equal? (interp `(cond
+                         [0 1]
+                         [0 2]
+                         [1 3])) 3)
+(check-equal? (interp `(cond
+                         [0 1]
+                         [else 4])) 4)
+(check-equal? (interp `(cond
+                         [0 1]
+                         [0 2]
+                         [0 3]
+                         [0 4]
+                         [1 5])) 5)
+(check-equal? (interp `(cond
+                         [0 1]
+                         [0 2]
+                         [0 3]
+                         [0 4]
+                         [0 5]
+                         [else 6])) 6)
