8f9bffde9d650f1087532188087a7a97976b810e
lnargang <luciano.nargang@gmail.com>
2026-02-09 15:12:27 -0500

    update A3

--- A3/A3.rkt
+++ A3/A3.rkt
@@ -6,30 +6,35 @@
 ; If all of the expressions evaluate to a non-#false value, all produces the list of the results not including any values which are #true; otherwise it produces #false.
 ; The all form should short-circuit; that is, this expression should not print anything: (all #false (print 'hi))
 
-
-
-(define-syntax fcons
+(define-syntax all-helper
   (syntax-parser
-    [(_ e es)
-     #'(if (equal? es #f) #f (cons e es))]))
+    [(_ e1 es ...)
+     #'(let ([eval e1])
+         (if eval
+             (cons eval (all-helper es ...))
+             (cons #f empty)))]
+    [(_)
+     #'empty]))
 
 (define-syntax all
   (syntax-parser
-    [(_ e1 es ...)
-    #'(let [(eval e1)]
-          (if eval
-               (if (equal? eval #t)
-                   (all es ...)
-                   (fcons eval (all es ...)))
-              #f))]
-      [(_) #''()]))
+    [(_ es ...)
+     #'(let ([output (all-helper es ...)])
+         (if (memv #f output)
+             #f
+             (filter (lambda (x) (not (eqv? #t x))) output)))]))
 
 
+(check-equal? (all #t) '())
+(check-equal? (all #f) #f)
+(check-equal? (all #f (print 'hi)) #f)
 (check-equal? (all #f 1 2 3) #f)
 (check-equal? (all 1 2 3 #f) #f)
 (check-equal? (all #t 1 2 3) '(1 2 3))
-(check-equal? (all #f (print 'hi)) #f)
-(check-equal? (all) '())
+(check-equal? (all #t 1 #t 2 #t 3) '(1 2 3))
+(check-equal? (all 1 2 3 4 5 6 7 8 9 #f) #f)
+(check-equal? (all 1 2 3 4 5 #t #f 6 7 8 9 #t #f) #f)
+; (check-equal? (all (print 'hi) #f (print 'hello)) #f)
 
 
 
@@ -38,7 +43,6 @@
 ; The form creates a struct type definition whose constructor ensures that the respective field values satisfy the named predicate.
 
 (begin-for-syntax
-  (require racket)
   (define-syntax-class (fld sname)
     (pattern (i:id c p:id)
       #:fail-when (not (equal? (syntax->datum #'c) ':))
@@ -55,8 +59,7 @@
       #:with (ids ...) #'(f.i ...)
       #:with (preds ...) #'(f.p ...)
       #:with (acc-names ...) #'(f.acc-name ...)
-      #:with (idx ...) #`#,(build-list (length (syntax->list #'(f ...))) add1)))
-  )
+      #:with (idx ...) #`#,(build-list (length (syntax->list #'(f ...))) add1))))
 
 (define-syntax struct/con
   (syntax-parser
@@ -69,15 +72,22 @@
                     (format "~a failed predicate ~a" fs.ids fs.preds)))
            ...
            (vector 'name fs.ids ...))
-         
          (define (fs.acc-names v)
            (vector-ref v fs.idx))
-         ...)
-     ]))
+         ...)]))
 
-
-(struct/con posn [{x : number?} {y : number?}])
+(struct/con posn [(x : number?) (y : number?)])
 (define p (posn 1 2))
 (check-equal? (posn-x p) 1)
 (check-equal? (posn-y p) 2)
 (check-exn exn:fail? (thunk (posn-y (posn 1 #f))))
+
+(struct/con alist [(index : number?) (val : boolean?)])
+(define a (alist 1 #f))
+(check-equal? (alist-index a) 1)
+(check-equal? (alist-val a) #f)
+
+(struct/con pixel [(x : number?) (y : number?) (color : string?)])
+(define px (pixel 140 200 "red"))
+(check-equal? (pixel-x px) 140)
+(check-equal? (pixel-color px) "red")
