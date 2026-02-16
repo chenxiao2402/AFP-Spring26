21bf3c2c829fc1ee3a94f8affbdeedf5fd0af4bc
awsalmah <awsalmahasneh@gmail.com>
2026-02-16 16:49:35 -0500

    merged the defined-lang files

--- A4/a4-defined-lang.rkt
+++ A4/a4-defined-lang.rkt
@@ -2,24 +2,23 @@
 
 (require (for-syntax syntax/parse))
 
-(define define-expr?
-  (lambda (x)
-    (eqv? (car x) 'define)))
-
-(define not-define-expr?
-  (lambda (x)
-    (not (define-expr? x))))
-
 (define-syntax (my-module-begin stx)
   (syntax-parse stx
     [(_ form ...)
-     #'(#%plain-module-begin
-        (begin
-          (define exprs (list (syntax->datum #'form) ...))
-          (define defs (filter define-expr? exprs))
-          (define non-defs (filter not-define-expr? exprs))
-          (define prog (append defs non-defs))
-          (println prog)))]))
+
+     (define define-expr?
+       (lambda (x)
+         (eqv? (car (syntax->datum x)) 'define)))
+
+     (define not-define-expr?
+       (lambda (x)
+         (not (define-expr? x))))
+     
+     (define exprs (syntax->list #'(form ...)))
+     (define defs (filter define-expr? exprs))
+     (define non-defs (filter not-define-expr? exprs))
+     
+     (datum->syntax stx (cons '#%plain-module-begin (append defs non-defs)))]))
 
 (provide (rename-out [my-module-begin #%module-begin])
          (except-out (all-from-out racket) #%module-begin))
