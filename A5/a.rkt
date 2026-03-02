923fa4ff9f5a9119e1f54839a47b0948846e6f59
Xiao Chen <chenxiao2402@gmail.com>
2026-03-01 20:46:49 -0500

    finish A5

--- A5/a.rkt
+++ A5/a.rkt
@@ -1,38 +1,8 @@
 ;; a.rkt
 #lang racket
-(require (for-syntax syntax/parse racket/match))
-(provide (rename-out [mb #%module-begin]) #%app + #%datum let-values let)
-
-(begin-for-syntax
-  (define-syntax-class exp #:datum-literals (+ let-values Whatever let)
-    (pattern (+ a 0)
-      #:when (not (symbol? (syntax->datum #'a)))
-      #:with code #'a)
-    (pattern (+ 0 a)
-      #:when (not (symbol? (syntax->datum #'a)))
-      #:with code #'a)
-    (pattern (let-values ([a] e) b)
-      #:when (begin
-               (when #f (println (syntax->datum #'a)) (println (syntax->datum #'b)))
-               (eqv? (syntax->datum #'a) (syntax->datum #'b)))
-      #:with code #'e)
-    ;q1
-    (pattern (let ([var:id] rhs:number) b)
-      #:with code (subst #'b #'var #'rhs))
-
-    (pattern (op a:number ...)
-      #:when (memv (syntax->datum #'op) '(+ - * /))
-      #:with code (begin
-                    (define args (syntax->datum #'(a ...)))
-                    (define rator (match (syntax->datum #'op)
-                                    ['+ +]
-                                    ['- -]
-                                    ['* *]
-                                    ['/ /]))
-                    (datum->syntax #'here (apply rator args))))
-    
-    (pattern Whatever #:with code #'(void))
-    (pattern a #:with code #'a)))
+(require (for-syntax syntax/parse racket/match ))
+(provide (rename-out [my-module-begin #%module-begin])
+         (except-out (all-from-out racket) #%module-begin))
 
 
 (begin-for-syntax
@@ -40,18 +10,94 @@
     (let loop ([stx bdy])
       (syntax-parse stx
         [x2:id #:when (free-identifier=? #'x2 var) rhs]
-        #;[(f arg arg2)
-           #`(#,(loop #'f) #,(loop #'arg) #,(loop #'arg2))]
-        ; fix the below case, it breaks when there is a second let like
-        ; (let ([a] 5) (+ a a a a (let ([b] 1) b)))
-        ; (let ([b] 1) (let ([a] 5) a))
         [(f arg ...)
-         (append (list #'#%app)
-                 (for/list ([i (syntax->list stx)])
-                   (loop i)))]
-        [_ stx]))))
+         (datum->syntax stx
+           (for/list ([i (syntax->list stx)])
+             (loop i))
+           stx)]
+        [_ stx])))
+  
+  (define (number-list? lst)
+    (and (list? lst)
+         (for/and ([x lst]) (number? x))))
 
-(define-syntax (mb stx)
+  (define-syntax-class exp #:datum-literals (+ - * / let-values Whatever let lambda)
+    ; Lab 5: Adding-zero Elimination
+    (pattern (+ a 0)
+      #:when (not (symbol? (syntax->datum #'a)))
+      #:with code #'(if (number? a) a (+ a 0)))
+    (pattern (+ 0 a)
+      #:when (not (symbol? (syntax->datum #'a)))
+      #:with code #'(if (number? a) a (+ 0 a)))
+    
+    ; Lab 5: Let-Values Elimination
+    (pattern (let-values ([a] e) b)
+      #:when (begin
+               (when #f (println (syntax->datum #'a)) (println (syntax->datum #'b)))
+               (eqv? (syntax->datum #'a) (syntax->datum #'b)))
+      #:with code #'e)
+
+
+    ; Assignment 5, Q1: Constant Propagation
+    (pattern (let ([var:id rhs:number]) b)
+      #:with code (syntax-parse (subst #'b #'var #'rhs)
+                    [e:exp #'e.code]))
+
+    ; Assignment 5, Q2: Redundant Variable Elimination
+    (pattern (let ([var:id rhs:id]) b)
+      #:with code (syntax-parse (subst #'b #'var #'rhs)
+                    [e:exp #'e.code]))
+
+    ; Assignment 5, Q3: Constant Folding
+    (pattern (op a:exp ...)
+      #:when (memv (syntax->datum #'op) '(+ - * /))
+      #:with code (let ([args (map syntax->datum (syntax->list #'(a.code ...)))])
+                    (cond
+                      [(number-list? args)
+                       (define rator (match (syntax->datum #'op)
+                                       ['+ +]
+                                       ['- -]
+                                       ['* *]
+                                       ['/ /]))
+                       (datum->syntax #'here (apply rator args))]
+                      [else #'(op a.code ...)])))
+  
+  
+
+    ; Assignment 5, Q4: Lambda to Let
+    (pattern ((lambda (var:id) body) arg)
+      #:with code (syntax-parse #'(let ([var arg]) body)
+                    [e:exp #'e.code]))
+
+
+    (pattern Whatever #:with code #'(void))
+    (pattern (cmd:exp arg:exp ...) #:with code #'(cmd.code arg.code ...))
+    (pattern a #:with code #'a)))
+
+
+
+(define-syntax (my-module-begin stx)
   (syntax-parse stx
-    [(_ aa:exp ...)
-     #'(#%module-begin aa.code ...)]))
+    [(_ form ...)
+     #'(#%plain-module-begin
+        (handle-form form) ...
+        (displayln ""))]))
+
+(define-syntax (handle-form stx)
+  (syntax-parse stx #:datum-literals (require provide)
+    [(_ (require arg ...))
+     #'(require arg ...)]
+    [(_ (provide arg ...))
+     #'(provide arg ...)]
+    [(_ aa:exp)
+      #'(begin
+              (displayln "")
+              (display "orignal expr: ")
+              (displayln (syntax->datum (syntax aa)))
+              (display "optimized expr: ")
+              (displayln (syntax->datum (syntax aa.code)))
+              (let ([result aa.code])
+                (when (not (void? result))
+                  (displayln result))))]
+    [(_ other) #'other]))
+
--- A5/b.rkt
+++ A5/b.rkt
@@ -1,13 +1,20 @@
 ;; b.rkt
 #lang s-exp "a.rkt"
+
+(require rackunit)
+
 Whatever ;; no errors
 (+ 10 20)
-; (let-values ([x] e) x) -> e
-
-(let-values ([x] 3) x)
-(let ([a] 5) a)
-(let ([a] 5) (+ a a a a))
-; (let ([a] 5) (+ a a a a (let ([b] 1) b)))
-; (let ([b] 1) (let ([a] 5) a))
 (+ 11)
 (+ 11 2 30 40)
+
+(let-values ([x] 3) x)
+(let ([a 5]) a)
+(let ([a 5]) (+ a a a a))
+(let ([a 5]) (+ a a a a (let ([b 1]) b)))
+(let ([b 1]) (let ([a 5]) a))
+
+((lambda (x) (+ 2 x)) 17)
+
+
+(check-exn exn:fail? (thunk (+ 'h 0)))
