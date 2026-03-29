7999d7bacf47dc321166a79240dd1a5600ea6f67
awsalmah <awsalmahasneh@gmail.com>
2026-03-29 00:07:29 -0400

    Updated the lang so it doesnt run without Staff

--- Final/impl/lang.rkt
+++ Final/impl/lang.rkt
@@ -1,9 +1,49 @@
 #lang racket
-
 (require (for-syntax syntax/parse))
+
+(define-syntax (process-staff stx)   
+  (syntax-parse stx
+    [(_) #'(void)]
+    [(_ forms ...) ; need to check if Staff appears in the forms and return an error message accordingly
+     #'(begin forms ...)]
+    ))
 
 (define-syntax (mb stx)
   (syntax-parse stx
-    [(_ forms ...) #'(#%module-begin "Welcome to Forte" forms ...)]))
+    #:datum-literals (Staff)
+    [(_)
+     #'(#%module-begin)]
+    [(_ (Staff forms ...))
+     #'(#%module-begin (process-staff forms ...))]
+    [(_ (Staff forms-0 ...)
+        (Staff forms-1 ...)
+        (Staff forms-2 ...) ...)
+     #'(#%module-begin (error "Can't have more than 1 staff"))]
+    [_
+     #'(#%module-begin (error "Can't start a program without a staff"))]
+    ))
 
-(provide (rename-out [mb #%module-begin]))
+
+(define-syntax (datum stx)
+  (syntax-parse stx
+    [(_ . n) #'(#%datum . n)]))
+
+; just a normal function definition for now
+(define-syntax (def stx)
+  (syntax-parse stx
+    [(_ (f:id arg:id) e)
+     #'(define (f arg) e)]))
+
+; just a normal variable def for now
+(define-syntax (defv stx)
+  (syntax-parse stx
+    [(_ var:id e)
+     #'(define var e)]))
+
+; just a normal app yk what it is 
+(define-syntax (app stx)
+  (syntax-parse stx
+    [(_ f:id arg:expr ...) #'(#%app f arg ...)]))
+
+
+(provide + def defv quote (rename-out [datum #%datum] [app #%app] [mb #%module-begin] ))
--- Final/impl/prog-1.rkt
+++ Final/impl/prog-1.rkt
@@ -1,1 +1,13 @@
 #lang s-exp "lang.rkt"
+#;#;
+(Staff 42
+       24
+       "a"
+       (plus 42 24)
+       (def (addd x) (plus x 1))
+       (defv x 5)
+       241
+       #;x)
+(Staff (defv Staff 5)
+       'a
+       Staff) 
